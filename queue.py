57840b6668a30581f63826233a0a606cfd4ad0c3
starkster7781 <72255636+starkster7781@users.noreply.github.com>
2020-10-03 16:40:13 +0530

    This is the 3rd problem !!

---
+++ queue.py
@@ -0,0 +1,7 @@
+#Write a Python program to create a queue and display all the members and size of the queue.
+
+#Sample Output:
+#Members of the queue:                                                                                         
+#0 1 2 3                                                                                                       
+#Size of the queue:                                                                                            
+#4 
