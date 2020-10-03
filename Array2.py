23504b46f904d7b4b329d752a4eec00399ce59e2
starkster7781 <72255636+starkster7781@users.noreply.github.com>
2020-10-03 16:25:28 +0530

    This is the second problem

---
+++ Array2.py
@@ -0,0 +1,5 @@
+#Write a Python program to convert an array to an ordinary list with the same items.
+#Output
+#Original array: array('i', [1, 3, 5, 3, 7, 1, 9, 3])                   
+#Convert the said array to an ordinary list with the same items:        
+#[1, 3, 5, 3, 7, 1, 9, 3] 
