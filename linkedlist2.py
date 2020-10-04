d695bfe1c862311a0364b423f187fad018127a3e
starkster7781 <72255636+starkster7781@users.noreply.github.com>
2020-10-04 12:54:20 +0530

    This is the 5th Problem !!!

---
+++ linkedlist2.py
@@ -0,0 +1,4 @@
+#Given a liked list and a key to be deleted. Delete the last occurrence of key from linked. The list may have duplicates.
+
+#Input:   1->2->3->5->2->10, key = 2
+#Output:  1->2->3->5->10
