f069d35e5658d3d2caabbec19841f07e3612d496
AishwaryaEmani <67262233+AishwaryaEmani@users.noreply.github.com>
2020-10-10 12:15:35 +0530

    updated the soliuton

--- Soluton_array1.py
+++ Soluton_array1.py
@@ -1,7 +1,7 @@
-list1 = []
+list = []
 number = int(input("enter num"))
 for i in range(number):
     arr = int(input())
-    list1.append(arr)  
-list1.reverse()
-print(list1) 
+    list.append(arr)  
+list.reverse()
+print(list) 
